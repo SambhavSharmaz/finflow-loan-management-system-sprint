2cefcea2e8c0279afe01b6c88bab0a84f0d7ee01
sambhav sharma <118333908+SambhavSharmaz@users.noreply.github.com>
2026-03-23 15:24:20 +0530

    Initial commit

---
+++ src/test/java/com/capgemini/finflow_loan_management_system/FinFlowLoanManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.finflow_loan_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinFlowLoanManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
